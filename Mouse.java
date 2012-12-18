2ccdb9aeb41495621bc0041a8c41cd0e7ccad786
mdq3 <mdq3@1015px.(none)>
2012-12-18 14:52:18 +0000

    Committer: mdq3 <mdq3@1015px.(none)> 	new file:   Mouse.java

---
+++ Mouse.java
@@ -0,0 +1,41 @@
+/**
+ * Class that is responsible for the creation and behaviour of a mouse.
+ * 
+ * @author Michael Quested
+ * @version 1.0
+ * @date 2012/12/18
+ */ 
+public abstract class Mouse{
+
+	protected int currentPosX, currentPosY;
+	protected boolean foundCheese;
+	protected Maze maze;
+	
+	/**
+	 * Constructor for class Mouse.
+	 * 
+	 * @param maze The maze that the mouse will be navigating.
+	 */ 
+	public Mouse(Maze maze){
+		this.maze = maze;
+	}
+	
+	/**
+	 * Move the mouse throughout the maze.
+	 */ 
+	public abstract void move();
+	
+	/**
+	 * Begin the mouse's search for the cheese.
+	 */ 
+	public void go()throws InterruptedException{
+		while(!foundCheese){
+			maze.restoreCell(currentPosX, currentPosY);
+			move();
+			maze.updateMouse(currentPosX, currentPosY);
+			if(maze.cheeseIsFound()){
+				foundCheese = true;
+			}
+		}
+	}
+}
