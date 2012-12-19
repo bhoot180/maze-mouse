7735edc7f955709b8ccdf35d9ed2371952ac3998
mdq3 <bruteforks@gmail.com>
2012-12-19 11:48:47 +0000

    Changes to be committed: 	modified:   Maze.java 	modified:   MazeGUI.java 	modified:   Mouse.java 	modified:   RecursiveBacktracker.java 	modified:   WallFollower.java
    Corrected indentation issues

--- MazeGUI.java
+++ MazeGUI.java
@@ -11,25 +11,25 @@ import java.awt.event.*;
  * @date 2012/12/17
  */
 public class MazeGUI{
-	
+ 
     private JFrame frame;
     private Container contentPane;
     private JPanel panel;
     private JButton[][] cells;
     private Maze maze;
     
-    /**
-     * Constructor for objects of class MazeGUI. 
-     * 
-     * @param width The grid width of the maze
-     * @param depth The grid depth of the maze
-     */
+ /**   
+  * Constructor for objects of class MazeGUI. 
+  * 
+  * @param width The grid width of the maze
+  * @param depth The grid depth of the maze
+  */
     public MazeGUI(Maze maze){
-		this.maze = maze;
-		cells = new JButton[maze.getWidth()][maze.getDepth()];
-		makeFrame();
-		frame.pack();
-		frame.setLocationRelativeTo(null);
+        this.maze = maze;
+        cells = new JButton[maze.getWidth()][maze.getDepth()];
+        makeFrame();
+        frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
     
@@ -37,7 +37,7 @@ public class MazeGUI{
      * Setup the frame and its contents.
      */ 
     private void makeFrame(){
-        frame = new JFrame("Maze Mouse");
+            frame = new JFrame("Maze Mouse");
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setJMenuBar(makeMenuBar());
         contentPane = frame.getContentPane();
@@ -48,153 +48,153 @@ public class MazeGUI{
         panel.setMinimumSize(new Dimension(maze.getWidth()*8, maze.getDepth()*8));
         panel.setMaximumSize(new Dimension(maze.getWidth()*8, maze.getDepth()*8));
         frame.setResizable(false);
-	}
-	
-	public void loadMaze(){
-		SwingWorker mazeRenderer = new SwingWorker<Void, Void>(){
-		    @Override
-		    public Void doInBackground(){
-				renderMaze();
-		        return null;
-			}
-			
-			@Override
-			public void done(){
-				contentPane.revalidate();
-			}
-		};
-		mazeRenderer.execute();
-	}
-	
-	/**
-	 * Iterate through the coordinates of the maze and render each cell
-	 * appropriately.
-	 */ 
+    }
+ 
+    public void loadMaze(){
+        SwingWorker mazeRenderer = new SwingWorker<Void, Void>(){
+            @Override
+            public Void doInBackground(){
+                renderMaze();
+                return null;
+    }
+   
+            @Override
+            public void done(){
+                contentPane.revalidate();
+            }
+        };
+        mazeRenderer.execute();
+    }
+ 
+    /**
+     * Iterate through the coordinates of the maze and render each cell
+     * appropriately.
+     */ 
     private void renderMaze(){
-		for(int y = 0; y < maze.getDepth(); y++){
-			for(int x = 0; x < maze.getWidth(); x++){
-				final int col = x;
-				final int row = y;
-				final JButton cell = new JButton();
-				cell.setPreferredSize(new Dimension(8, 8));
-				cell.setBorderPainted(false);
-				if(maze.isPassage(x, y)){
-					cell.setBackground(Color.WHITE);
-					cell.addMouseListener(new MouseListener(){
-						Color oldColor;
-						public void mouseEntered(MouseEvent e){
-							oldColor = cell.getBackground();
-							if(!maze.cheeseIsSet){
-								cell.setBackground(Color.YELLOW);
-							}
-							else if(!maze.mouseIsSet){
-								cell.setBackground(Color.BLUE);
-							}
-						}
-						public void mouseExited(MouseEvent e){
-								cell.setBackground(oldColor);
-						}
-						public void mouseClicked(MouseEvent evt){
-							if(!maze.cheeseIsSet){
-								oldColor = Color.YELLOW;
-								maze.setReset();
-								maze.setCheese(col, row);
-							}
-							else if(!maze.mouseIsSet){
-								oldColor = Color.WHITE;
-								SwingWorker runMouse = new SwingWorker<Void, Void>(){
-									@Override
-									public Void doInBackground(){
-										try{
-											maze.setMouse(col, row);
-										}catch(InterruptedException e){}
-										return null;
-									}
-								};
-								runMouse.execute();
-							}
-						}  
-					    public void mousePressed(MouseEvent evt){}  
-					    public void mouseReleased(MouseEvent evt){}
-					});
-				    cells[x][y] = cell;
-				}else{
-					cell.setBackground(Color.BLACK);
-					cell.setEnabled(false);
-					cells[x][y] = cell;
-				}
-				panel.add(cell);
-			}
-		}
-	}
-	
-	/**
-	 * Set the cheese on the grid.
-	 * 
-	 * @param x The grid position x of the cheese
-	 * @param y The grid position y of the cheese
-	 */ 
-	public void setCheese(int x, int y){
-		cells[x][y].setBackground(Color.YELLOW);	
-	}
-	
-	/**
-	 * Update the mouse's position on the grid.
-	 * 
-	 * @param x The grid position x of the mouse
-	 * @param y The grid position y of the mouse
-	 */ 
-	public void updateMouse(int x, int y){
-		cells[x][y].setBackground(Color.BLUE);
-		cells[x][y].revalidate();
-	}
-	
-	/**
-	 * Restore the cell to represent an empty passage.
-	 * 
-	 * @param x The grid position x of the passage
-	 * @param y The grid position y of the passage
-	 */ 
-	public void restoreCell(int x, int y){
-		cells[x][y].setBackground(Color.WHITE);
-		cells[x][y].revalidate();
-	}
-	
-	/**
-	 * Make the menubar for the frame.
-	 */  
-	private JMenuBar makeMenuBar(){
-		JMenuBar menuBar = new JMenuBar();
-		
-		//---------------------- The Maze Menu -------------------------
-		
-		JMenu mazeMenu = new JMenu("Maze");
+        for(int y = 0; y < maze.getDepth(); y++){
+            for(int x = 0; x < maze.getWidth(); x++){
+                final int col = x;
+                final int row = y;
+                final JButton cell = new JButton();
+                cell.setPreferredSize(new Dimension(8, 8));
+                cell.setBorderPainted(false);
+                if(maze.isPassage(x, y)){
+                    cell.setBackground(Color.WHITE);
+                    cell.addMouseListener(new MouseListener(){
+                        Color oldColor;
+                        public void mouseEntered(MouseEvent e){
+                            oldColor = cell.getBackground();
+                            if(!maze.cheeseIsSet){
+                                cell.setBackground(Color.YELLOW);
+                            }
+                            else if(!maze.mouseIsSet){
+                                cell.setBackground(Color.BLUE);
+                            }
+                        }
+                        public void mouseExited(MouseEvent e){
+                            cell.setBackground(oldColor);
+                        }
+                        public void mouseClicked(MouseEvent evt){
+                            if(!maze.cheeseIsSet){
+                                oldColor = Color.YELLOW;
+                                maze.setReset();
+                                maze.setCheese(col, row);
+                            }
+                            else if(!maze.mouseIsSet){
+                                oldColor = Color.WHITE;
+                                SwingWorker runMouse = new SwingWorker<Void, Void>(){
+                                    @Override
+                                    public Void doInBackground(){
+                                        try{
+                                            maze.setMouse(col, row);
+                                        }catch(InterruptedException e){}
+                                        return null;
+                                    }
+                                };
+                                runMouse.execute();
+                            }
+                        }  
+                        public void mousePressed(MouseEvent evt){}  
+                        public void mouseReleased(MouseEvent evt){}
+                    });
+                    cells[x][y] = cell;
+                }else{
+                    cell.setBackground(Color.BLACK);
+                    cell.setEnabled(false);
+                    cells[x][y] = cell;
+                }
+                panel.add(cell);
+            }
+        }
+    }
+ 
+    /**
+     * Set the cheese on the grid.
+     * 
+     * @param x The grid position x of the cheese
+     * @param y The grid position y of the cheese
+     */ 
+    public void setCheese(int x, int y){
+        cells[x][y].setBackground(Color.YELLOW); 
+    }
+ 
+    /**
+     * Update the mouse's position on the grid.
+     * 
+     * @param x The grid position x of the mouse
+     * @param y The grid position y of the mouse
+     */ 
+    public void updateMouse(int x, int y){
+        cells[x][y].setBackground(Color.BLUE);
+        cells[x][y].revalidate();
+    }
+ 
+    /**
+     * Restore the cell to represent an empty passage.
+     * 
+     * @param x The grid position x of the passage
+     * @param y The grid position y of the passage
+     */ 
+    public void restoreCell(int x, int y){
+        cells[x][y].setBackground(Color.WHITE);
+        cells[x][y].revalidate();
+    }
+ 
+    /**
+     * Make the menubar for the frame.
+     */  
+    private JMenuBar makeMenuBar(){
+        JMenuBar menuBar = new JMenuBar();
+  
+        //---------------------- The Maze Menu -------------------------
+  
+        JMenu mazeMenu = new JMenu("Maze");
         menuBar.add(mazeMenu);
 
         // Reset item
         final JMenuItem resetItem = new JMenuItem("Reset");
         resetItem.addActionListener(new ActionListener(){
                 public void actionPerformed(ActionEvent e){ 
-					SwingWorker resetWorker = new SwingWorker<Void, Void>(){
-						@Override
-						public Void doInBackground(){
-							resetItem.setEnabled(false);
-							maze.reset();
-							renderMaze();
-							frame.revalidate();
-							return null;
-						}
-						
-						@Override
-						public void done(){
-							resetItem.setEnabled(true);
-						}
-					};
-					panel.removeAll();
-					frame.revalidate();
-					frame.repaint();
-					resetWorker.execute();
-				}
+                    SwingWorker resetWorker = new SwingWorker<Void, Void>(){
+                        @Override
+                        public Void doInBackground(){
+                            resetItem.setEnabled(false);
+                            maze.reset();
+                            renderMaze();
+                            frame.revalidate();
+                            return null;
+                        }
+      
+                        @Override
+                        public void done(){
+                            resetItem.setEnabled(true);
+                        }
+                    };
+                    panel.removeAll();
+                    frame.revalidate();
+                    frame.repaint();
+                    resetWorker.execute();
+                }
             });
         mazeMenu.add(resetItem);
         
@@ -214,11 +214,11 @@ public class MazeGUI{
         JMenuItem instructionsItem = new JMenuItem("Instructions");
         instructionsItem.addActionListener(new ActionListener(){
                 public void actionPerformed(ActionEvent e){ 
-					infoMessage("Once maze has been generated, click on a cell\n" +
-								 "to place the cheese in the maze.\n\n" +
-								 "Then place the mouse and watch it hunt for the\ncheese.",
-								 "Instructions");
-				}
+                    infoMessage("Once maze has been generated, click on a cell\n" +
+                                "to place the cheese in the maze.\n\n" +
+                                "Then place the mouse and watch it hunt for the\ncheese.",
+                                "Instructions");
+                }
             });
         helpMenu.add(instructionsItem);
         
@@ -226,24 +226,24 @@ public class MazeGUI{
         JMenuItem aboutItem = new JMenuItem("About");
         aboutItem.addActionListener(new ActionListener(){
                 public void actionPerformed(ActionEvent e){ 
-					infoMessage("MAZE MOUSE\n\n" +
-								"author: Michael Quested\n" +
-								"date: 2012/12/18\n" +
-								"version: 1.0\n\n" +
-								"The project source can be found at:\n" +
-								"https://github.com/mdq3/maze-mouse.git", 
-								"About");
-				}
+                    infoMessage("MAZE MOUSE\n\n" +
+                                "author: Michael Quested\n" +
+                                "date: 2012/12/18\n" +
+                                "version: 1.0\n\n" +
+                                "The project source can be found at:\n" +
+                                "https://github.com/mdq3/maze-mouse.git", 
+                                "About");
+                }
             });
         helpMenu.add(aboutItem);
-		
-		return menuBar;
-	}
-	
-	/**
+        return menuBar;
+    }
+ 
+    /**
      * Create a popup information message.
      */
     protected void infoMessage(String info, String title){
         JOptionPane.showMessageDialog(frame, info, title, JOptionPane.INFORMATION_MESSAGE);
     }
 }
+
--- WallFollower.java
+++ WallFollower.java
@@ -10,93 +10,89 @@ import java.util.Random;
  */ 
 public class WallFollower extends Mouse{
 
-	private String bearing;
-	
-	/**
-	 * Constructor for class WallFollower.
-	 * 
-	 * @param maze The maze that the mouse will be navigating.
-	 */ 
-	public WallFollower(Maze maze){
-		super(maze);
-		Random random = new Random();
-		String[] directions = {"NORTH", "SOUTH", "EAST", "WEST"};
-		bearing = directions[random.nextInt(4)];		
-	}
-	
-	/**
-	 * Move the mouse throughout the maze. Using a wall following 
-	 * algorithm, the mouse always follows the wall on its right-
-	 * hand side.
-	 */ 
-	public void move(){
-		if(bearing.equals("NORTH")){
-			if(maze.coordinates[currentPosX+1][currentPosY]){
-				bearing = "EAST";
-				currentPosX += 1;
-			}
-			else if(maze.coordinates[currentPosX][currentPosY-1]){
-				currentPosY -= 1;
-			}
-			else if(maze.coordinates[currentPosX-1][currentPosY]){
-				bearing = "WEST";
-				currentPosX -= 1;
-			}
-			else{
-				bearing = "SOUTH";
-				currentPosY += 1;
-			}
-		}
-		else if(bearing.equals("EAST")){
-			if(maze.coordinates[currentPosX][currentPosY+1]){
-				bearing = "SOUTH";
-				currentPosY += 1;
-			}
-			else if(maze.coordinates[currentPosX+1][currentPosY]){
-				currentPosX += 1;
-			}
-			else if(maze.coordinates[currentPosX][currentPosY-1]){
-				bearing = "NORTH";
-				currentPosY -= 1;
-			}
-			else{
-				bearing = "WEST";
-				currentPosX -= 1;
-			}
-		}
-		else if(bearing.equals("SOUTH")){
-			if(maze.coordinates[currentPosX-1][currentPosY]){
-				bearing = "WEST";
-				currentPosX -= 1;
-			}
-			else if(maze.coordinates[currentPosX][currentPosY+1]){
-				currentPosY += 1;
-			}
-			else if(maze.coordinates[currentPosX+1][currentPosY]){
-				bearing = "EAST";
-				currentPosX += 1;
-			}
-			else{
-				bearing = "NORTH";
-				currentPosY -= 1;
-			}
-		}
-		else if(bearing.equals("WEST")){
-			if(maze.coordinates[currentPosX][currentPosY-1]){
-				bearing = "NORTH";
-				currentPosY -= 1;
-			}
-			else if(maze.coordinates[currentPosX-1][currentPosY]){
-				currentPosX -= 1;
-			}
-			else if(maze.coordinates[currentPosX][currentPosY+1]){
-				bearing = "SOUTH";
-				currentPosY += 1;
-			}
-			else{
-				bearing = "EAST";
-				currentPosX += 1;
-			}			
-		}
-	}
+ private String bearing;
+ 
+    /**
+     * Constructor for class WallFollower.
+     * 
+     * @param maze The maze that the mouse will be navigating.
+     */ 
+    public WallFollower(Maze maze){
+        super(maze);
+        Random random = new Random();
+        String[] directions = {"NORTH", "SOUTH", "EAST", "WEST"};
+        bearing = directions[random.nextInt(4)];  
+    }
+ 
+    /**
+     * Move the mouse throughout the maze. Using a wall following 
+     * algorithm, the mouse always follows the wall on its right-
+     * hand side.
+     */ 
+    public void move(){
+        if(bearing.equals("NORTH")){
+            if(maze.coordinates[currentPosX+1][currentPosY]){
+                bearing = "EAST";
+                currentPosX += 1;
+            }
+            else if(maze.coordinates[currentPosX][currentPosY-1]){
+                currentPosY -= 1;
+            }
+            else if(maze.coordinates[currentPosX-1][currentPosY]){
+                bearing = "WEST";
+                currentPosX -= 1;
+            }else{
+                bearing = "SOUTH";
+                currentPosY += 1;
+            }
+        }
+        else if(bearing.equals("EAST")){
+            if(maze.coordinates[currentPosX][currentPosY+1]){
+                bearing = "SOUTH";
+                currentPosY += 1;
+                }
+            else if(maze.coordinates[currentPosX+1][currentPosY]){
+                currentPosX += 1;
+            }
+            else if(maze.coordinates[currentPosX][currentPosY-1]){
+                bearing = "NORTH";
+                currentPosY -= 1;
+            }else{
+                bearing = "WEST";
+                currentPosX -= 1;
+            }
+        }
+        else if(bearing.equals("SOUTH")){
+            if(maze.coordinates[currentPosX-1][currentPosY]){
+                bearing = "WEST";
+                currentPosX -= 1;
+            }
+            else if(maze.coordinates[currentPosX][currentPosY+1]){
+            currentPosY += 1;
+            }
+            else if(maze.coordinates[currentPosX+1][currentPosY]){
+                bearing = "EAST";
+                currentPosX += 1;
+            }else{
+                bearing = "NORTH";
+                currentPosY -= 1;
+            }
+        }
+        else if(bearing.equals("WEST")){
+            if(maze.coordinates[currentPosX][currentPosY-1]){
+                bearing = "NORTH";
+                currentPosY -= 1;
+            }
+            else if(maze.coordinates[currentPosX-1][currentPosY]){
+                currentPosX -= 1;
+            }
+            else if(maze.coordinates[currentPosX][currentPosY+1]){
+                bearing = "SOUTH";
+                currentPosY += 1;
+            }else{
+                bearing = "EAST";
+                currentPosX += 1;
+            }   
+        }
+    }
 }
